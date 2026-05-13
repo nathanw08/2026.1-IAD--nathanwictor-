1d7fcad25927fd1b0bad3f12de219ce16875e9b5
nathanw08 <nathanwictor05@gmail.com>
2026-05-12 22:51:44 -0300

    Delete cap 2 - ex4.py

--- cap 2 - ex4.py
+++
@@ -1,9 +0,0 @@
-# Passo 1: Solicita a temperatura ao usuário
-# Usamos float() porque a temperatura pode ter casas decimais
-celsius = float(input("Digite a temperatura em Celsius: "))
-
-# Passo 2: Aplica a fórmula de conversão
-fahrenheit = (celsius * 1.8) + 32
-
-# Passo 3: Mostra o resultado formatado
-print(f"{celsius}°C é igual a {fahrenheit}°F")
